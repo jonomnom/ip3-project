ce7bc9366bef0bcfb84ee881cee36461748bdc14
Tingwei Zhou <zhoutingwei1996@gmail.com>
2022-11-05 03:01:46 -0700

    add explore card

--- ip3-frontend/components/NFTCard/DigitalIPCard.tsx
+++ ip3-frontend/components/NFTCard/DigitalIPCard.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@nextui-org/react'
+import { Button, Divider } from '@nextui-org/react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Checkbox, DateRangePicker } from 'rsuite'
@@ -59,89 +59,30 @@ export default function DigitalIPCard({ nft }: Props) {
         </div>
       </Link>
 
-      <div className="flex w-full flex-col py-4">
-        <div className="flex w-full flex-col items-start justify-between px-4 py-2 font-medium">
-          {/* <div className="truncate pr-2 text-sm">
-            {`${nft.autorizeIP.collectionName} #${nft.autorizeIP.collectionTokenId}`}
-          </div> */}
-
-          <div className="flex items-center justify-center rounded-2xl border-[0.5px] border-black px-4 font-title text-sm">
-            Choose Avalibility
-          </div>
-
-          <div className="flex w-full flex-col pl-4 pt-2">
-            <Checkbox defaultSelected={true} size="xs">
-              <span className="pl-2 font-content text-xs font-semibold">
-                Authorize by time
-              </span>
-            </Checkbox>
-            <div className="flex items-center justify-start gap-2 bg-[#0004C3] bg-opacity-5 px-4 py-2">
-              <div className="text-xs text-[#0004C3]">Set Your Price</div>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                min="0"
-                step="0.001"
-                required
-                className="w-24 rounded-xl border border-[#0004C3] bg-[#0004C3] bg-opacity-5 pl-4 text-sm text-opacity-60"
-                // className={classNames(styles.inputStyles)}
-                // onChange={(e) => {
-                //   setPrice(e.target.value)
-                // }}
-                // value={price}
-              />
-              <div className="text-xs text-[#0004C3] opacity-80">
-                USDT / Day
-              </div>
-            </div>
-
-            <Checkbox defaultSelected={true} size="xs">
-              <span className="pl-2 font-content text-xs font-semibold">
-                Authorize by quantity
-              </span>
-            </Checkbox>
-
-            <div className="flex items-center justify-start gap-2 bg-[#0004C3] bg-opacity-5 px-4 py-2">
-              <div className="text-xs text-[#0004C3]">Set Your Price</div>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                min="0"
-                step="0.001"
-                required
-                className="w-24 rounded-xl border border-[#0004C3] bg-[#0004C3] bg-opacity-5 pl-4 text-sm text-opacity-60"
-                // className={classNames(styles.inputStyles)}
-                // onChange={(e) => {
-                //   setPrice(e.target.value)
-                // }}
-                // value={price}
-              />
-              <div className="text-xs text-[#0004C3] opacity-80">
-                USDT / Amount
-              </div>
-            </div>
-
-            <div className="pt-4">
-              <DateRangePicker
-                ranges={predefinedRanges}
-                disabledDate={beforeToday()}
-                placeholder="Duration"
-                style={{ width: 200 }}
-              />
+      <div className="flex w-full flex-col py-4 font-content">
+        <div className="flex items-center justify-between px-4">
+          <div className="flex flex-col items-start justify-between py-2 font-medium">
+            <div className="flex items-center justify-center rounded-2xl border-[0.5px] border-black px-4 font-title text-sm">
+              {`${nft.autorizeIP.collectionName} #${nft.autorizeIP.collectionTokenId}`}
             </div>
           </div>
 
-          {/* <div className="truncate pr-2 text-sm">
-            {`${nft.currentRentalPriceByDuration} USD / day`}
-          </div>
-          <div className="truncate pr-2 text-sm">
-            {`${nft.currentRentalPriceByAmount} USD / amount`}
-          </div> */}
-          <button className=" mt-4 flex items-center justify-center place-self-center rounded-2xl bg-black px-4 font-title text-sm text-white">
-            Authorize
+          <button className="flex items-center justify-center rounded-2xl bg-black px-4 pr-4 font-title text-sm text-white">
+            RENT
           </button>
+        </div>
+
+        <div className="pl-8 pt-2">
+          <div className="text-sm opacity-40">List Price for Authorization</div>
+          <div>{nft.currentRentalPriceByDuration} USDT/Day</div>
+          <div>{nft.currentRentalPriceByAmount} USDT/Amount</div>
+        </div>
+
+        <div className="mx-6 my-4 border-t border-black " />
+
+        <div className="pl-8">
+          <div>Owner</div>
+          <div>xxx</div>
         </div>
       </div>
     </div>
